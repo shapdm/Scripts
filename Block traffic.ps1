824b4fe2dcf19140ed4be2947484e7acdaae868b
ShapDm <shapkin.dmitry@gmail.com>
2024-06-18 17:08:08 +0300

    add two scripts

---
+++ Block traffic.ps1
@@ -0,0 +1,17 @@
+﻿Write-Host
+Write-Host "Список доступных действий" -BackgroundColor White -ForegroundColor Red
+Write-Host
+
+Write-Host "1. Создать блокирующее правило" -ForegroundColor Green
+Write-Host "2. Удалить блокирующее правило" -ForegroundColor Green
+Write-Host "3. Завершить" -ForegroundColor Green
+Write-Host
+
+$choice = Read-Host "Выберете пункт из списка"
+
+Switch($choice){
+  1{New-NetFirewallRule -Action Block -Profile Any -DisplayName "block traffic" -RemoteAddress Internet -Direction Outbound}
+  2{Remove-NetFirewallRule -DisplayName "block traffic"}
+  3{Write-Host "Завершить"; exit}
+    default {Write-Host "Некорректный ввод, попробуйте снова" -ForegroundColor Red}
+}
---
+++ date-changer.ps1
@@ -0,0 +1,26 @@
+﻿Set-ExecutionPolicy -ExecutionPolicy unrestricted
+
+Write-Host
+Write-Host "Список доступных действий" -ForegroundColor Green
+Write-Host
+
+Write-Host "1. Изменить дату для записи" -ForegroundColor Green
+Write-Host "2. Изменить дату на актуальную" -ForegroundColor Green
+Write-Host "3. Закрыть программу" -ForegroundColor Green
+Write-Host
+
+$choice = Read-Host "Выберете пункт из списка"
+
+Switch($choice){
+  1{
+    stop-service w32time
+    $date = read-host "Введите дату и время в формате ДД.ММ.ГГГГ ЧЧ:ММ" 
+    Set-Date $date
+  }
+  2{restart-service w32time}
+  3{Write-Host "Завершаем процессы, закрываем программу"; exit}
+    default {Write-Host "Некорректный ввод, попробуйте снова" -ForegroundColor Red}
+}
+
+Write-Host "Проверьте результат и нажмите на любую клавишу!"
+$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
